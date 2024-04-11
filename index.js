63e028d65b6fea77dbbb66b515af503a0a5611cf
NachoCheeseOfDoom <85528067+NachoCheeseOfDoom@users.noreply.github.com>
2024-04-11 16:48:25 -0600

    Finished instagram clone with stretch goals

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const feed = document.getElementById('page-feed')
+
 const posts = [
   {
     name: "Vincent van Gogh",
@@ -29,7 +31,56 @@ const posts = [
   },
 ];
 
-posts.forEach((element, index) => {
-    console.log((element = index === 0 ? element : null));
-});
-// console.log("hello");
+function render() {
+  feed.innerHTML = '';
+
+  posts.forEach((post, index) => {
+    feed.innerHTML += `
+<section class="user-info">
+            <img class="user-info__img" src="${post.avatar}" alt="Avatar profile picture">
+            <div>
+                <p class="avatar-name"><span class="bold-text">${post.name}</span></p>
+                <p class="avatar-location">${post.location}</p>
+            </div>
+        </section>
+        <section>
+            <img data-index="${index}" class="feed-post" src="${post.post}" alt="Instagram post">
+        </section>
+        <section class="interactions">
+            <div id="icon-group" class="icon-group">
+                <img data-index="${index}" class="likeImg" src="images/icon-heart.png" alt="like button"> 
+                <img src="images/icon-comment.png" alt="comment button">
+                <img src="images/icon-dm.png" alt="share button">
+            </div>
+            <p class="bold-text">${post.likes} likes</p>
+            <p><span class="bold-text">${post.username}</span> ${post.comment}</p>
+        </section>
+`
+  });
+
+  likeBtnListener()
+}
+
+function likeBtnListener() {
+
+  const likeBtns = document.getElementsByClassName('likeImg');
+  const feedPosts = document.getElementsByClassName('feed-post');
+
+  Array.from(likeBtns).forEach(likeBtn => {
+    likeBtn.addEventListener('click', (event) => {
+      const index = event.target.dataset.index;
+      posts[index].likes++;
+      render();
+    });
+  });
+
+  Array.from(feedPosts).forEach(feedPost => {
+    feedPost.addEventListener('dblclick', (event) => {
+      const index = event.target.dataset.index;
+      posts[index].likes++;
+      render();
+    });
+  });
+}
+
+render();
